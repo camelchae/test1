4e427d7177784614bc98ea2d9c3f6a3537c10fb5
camelchae <camelchae@gmail.com>
2023-02-27 09:58:13 +0900

    done

--- ES2DViewerClient/ES2DViewerClient.pro
+++ ES2DViewerClient/ES2DViewerClient.pro
@@ -1,4 +1,7 @@
-QT       += core gui
+include($$(VTBUILD_CORE_PATH)/VTBuildConf.pri)
+!exists($$(VTBUILD_CORE_PATH)/VTBuildConf.pri) : error("Can't find VTBuildConf.pri")
+
+QT       += core gui xml
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -7,6 +10,14 @@ CONFIG += c++11
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+ES_PRODUCT_DIR=C:/SVN/cleverone/product
+
+#import ES2DViewer library
+ES2DVIEWER_IMPORT_PATH = $$ES_PRODUCT_DIR/common/ESViewerCommon/ES2DViewer/ImportES2DViewer.pri
+!exists($$ES2DVIEWER_IMPORT_PATH) : error("Can't include $${ES2DVIEWER_IMPORT_PATH}")
+include($$ES2DVIEWER_IMPORT_PATH)
+
 
 SOURCES += \
     main.cpp \
@@ -17,6 +28,9 @@ HEADERS += \
 
 FORMS += \
     mainwindow.ui
+
+INCLUDEPATH += $${ES_PRODUCT_DIR}/common/
+INCLUDEPATH += $${ES_PRODUCT_DIR}/inc/
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- ES2DViewerClient/mainwindow.cpp
+++ ES2DViewerClient/mainwindow.cpp
@@ -1,11 +1,42 @@
+#include <QFile>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
+#include "ES2DViewerCommon.h"
+
+using namespace es2d;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    m_pViewerWidget = new CES2DViewer(this);
+
+    if(m_pViewerWidget)
+    {
+        QString strAppPath = qApp->applicationDirPath();
+        QString strSampleImage = "FeatureToastBulldogImg.png";
+
+        QString strSamplePath = QString("%1\\%2").arg(strAppPath).arg(strSampleImage);
+        //QString strSamplePath = "c:\\temp\\test.dcm";
+        //QString strSamplePath = "c:\\temp\\sample\\test_1.jpg";
+
+        if(false == QFile::exists(strSamplePath))
+        {
+            QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+            QString strSystemSamplePath = QString("%1\\sysnative\\%2").arg(env.value("systemroot")).arg(strSampleImage);
+
+            QFile::copy(strSystemSamplePath, strSamplePath);
+        }
+
+        dynamic_cast<CES2DViewer *>(m_pViewerWidget)->SetImage(strSamplePath);
+        dynamic_cast<CES2DViewer *>(m_pViewerWidget)->SetCurrentCommand(eSelect);
+
+        setCentralWidget(m_pViewerWidget);
+    }
+
 }
 
 MainWindow::~MainWindow()
